753f09cf73f6790d3e83efeebb80c216f77bd4bf
Anton K <i.openuser@yandex.ru>
2025-03-30 21:54:17 +0500

    open close search

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+const modal = document.querySelector('.search-model')
+const modalBtn = document.querySelector('.icon_search')
+const closeBtn = modal.querySelector('.search-close-switch')
+const inputsearch = modal.querySelector('#search-input')
+
+modalBtn.addEventListener('click', () => {
+	modal.style.display = 'block'
+	inputsearch.focus();
+})
+closeBtn.addEventListener('click', () => {
+	modal.style.display = 'none'
+})
+
+inputsearch.addEventListener('input', updateValue)
+function updateValue(e) {
+	console.log(e.target.value)
+}
