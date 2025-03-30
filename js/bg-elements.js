4fae986fca68aef36858320dbf64f21edc496e79
Anton K <i.openuser@yandex.ru>
2025-03-30 23:58:56 +0500

    test filter sort

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,10 +1,31 @@
 const bgElements = () => {
 	const elements = document.querySelectorAll('.set-bg')
 
-	for(let i = 0; i< elements.length; i++) {
-		const src = elements[i].dataset.setbg
-	
-		elements[i].style.backgroundImage = `url(${src})`
-	}
+	elements.forEach((elem) => {
+		// const src = elem.dataset.setbg
+		elem.style.backgroundImage = `url(${elem.dataset.setbg})`
+
+		// const array = [
+		// 	{
+		// 		id: 0,
+		// 		value: 100
+		// 	},
+		// 	{
+		// 		id: 2,
+		// 		value: 300
+		// 	},
+		// 	{
+		// 		id: 1,
+		// 		value: 200
+		// 	}
+		// ]
+		
+		// const newArray = array.filter( (item) => item <= 3 )
+
+		// const newArray = array.sort((a, b) => a.value - b.value)
+
+		// console.log(newArray);
+	})
+
 }
 bgElements()
