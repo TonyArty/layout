7df5873ad217996d7a1cc62e4fb280a2f1661207
Anton K <i.openuser@yandex.ru>
2025-03-31 00:51:33 +0500

    render TOP get ganres

--- js/mainData.js
+++ js/mainData.js
@@ -1,9 +1,45 @@
 const mainData = () => {
+
+	const renderAnimeList = (array, ganres) => {
+		console.log(array);
+		console.log(ganres);
+	}
+
+	const renderTopAnime = (array) => {
+		const wrapper = document.querySelector('.filter__gallery')
+		wrapper.innerHTML = ''
+		array.forEach((item) => {
+			// console.log(item);
+			wrapper.insertAdjacentHTML('afterbegin', `
+				<div class="product__sidebar__view__item set-bg mix" data-setbg="${item.image}">
+						<div class="ep">18 / ${item.rating}</div>
+						<div class="view">
+							<i class="fa fa-eye"></i> ${item.views}
+						</div>
+						<h5>
+							<a href="/anime-details.html">${item.title}</a>
+						</h5>
+				</div>
+			`)
+			wrapper.querySelectorAll('.set-bg').forEach((elem) => {
+				elem.style.backgroundImage = `url(${elem.dataset.setbg})`
+			})
+		})
+	}
+
 	fetch('https://anime-d494d-default-rtdb.firebaseio.com/anime.json').then((response) => {
 		return response.json()
 	})
 	.then((data) => {
-		console.log(data[3]);
+		const ganres = new Set()
+
+		renderTopAnime(data.sort((a, b) => b.views - a.views).slice(0, 5))
+
+		data.forEach((item) => {
+			ganres.add(item.ganre)
+		})
+		
+		renderAnimeList(data, ganres)
 	})
 }
 mainData()
