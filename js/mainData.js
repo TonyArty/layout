fc988e06140e34e1c12377d1946dbb1d3ea11efa
Anton K <i.openuser@yandex.ru>
2025-03-31 12:06:27 +0500

    swiper css - cdn to local

--- js/mainData.js
+++ js/mainData.js
@@ -1,8 +1,64 @@
 const mainData = () => {
 
 	const renderAnimeList = (array, ganres) => {
-		console.log(array);
-		console.log(ganres);
+		const wrapper = document.querySelector('.product .col-lg-8')
+		wrapper.innerHTML = ''
+		ganres.forEach((ganre) => {
+			const producBlock = document.createElement('div')
+			const listBlock = document.createElement('div')
+			const list = array.filter(item => item.ganre === ganre)
+
+			listBlock.classList.add('row')
+			producBlock.classList.add('mb-5')
+
+			producBlock.insertAdjacentHTML('afterbegin', `
+				<div class="row">
+						<div class="col-lg-8 col-md-8 col-sm-8">
+								<div class="section-title">
+										<h4>${ganre}</h4>
+								</div>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4">
+								<div class="btn__all">
+										<a href="/categories.html?ganre=${ganre}" class="primary-btn">View All <span class="arrow_right"></span></a>
+								</div>
+						</div>
+				</div>
+			`)
+
+			list.forEach(item => {
+				const tagsBlock = document.createElement('ul')
+				item.tags.forEach(tag => {
+					tagsBlock.insertAdjacentHTML('afterbegin', `
+						<li>${tag}</li>
+					`)
+				})
+				console.dir(tagsBlock)
+
+
+				listBlock.insertAdjacentHTML('afterbegin', `
+					<div class="col-lg-4 col-md-6 col-sm-6">
+							<div class="product__item">
+									<div class="product__item__pic set-bg" data-setbg="${item.image}">
+											<div class="ep">18 / ${item.rating}</div>
+											<div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+									</div>
+									<div class="product__item__text">
+											${tagsBlock.outerHTML}
+											<h5><a href="/anime-details.html?itemId=${item.id}">${item.title}</a></h5>
+									</div>
+							</div>
+					</div>
+				`)
+			})
+
+			producBlock.append(listBlock)
+			wrapper.append(producBlock)
+
+			wrapper.querySelectorAll('.set-bg').forEach((elem) => {
+				elem.style.backgroundImage = `url(${elem.dataset.setbg})`
+			})
+		})
 	}
 
 	const renderTopAnime = (array) => {
